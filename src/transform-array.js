9926f02dd718cb072d83d95e6aa3c6d1453a9216
Alina Posypaiko <alina.posypaiko@gmail.com>
2022-10-15 00:28:02 +0300

    feat: try to solve problem

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (Array.isArray(arr)) {
+    let newArr = arr;
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] === '--discard-next' && i != (newArr.length -1)) {
+        newArr.splice(i, 2);
+      } else if (newArr[i] === '--discard-next' && i === (newArr.length -1)) {
+        newArr.splice(i, 1);
+      } else if (newArr[i] === '--discard-prev' && i != 0) {
+        newArr.splice(--i, 2);
+      } else if (newArr[i] === '--discard-prev' && i === 0) {
+        newArr.splice(i, 1);
+      } else if (newArr[i] === '--double-next' && i != (newArr.length -1)) {
+        newArr.splice(i, 1, newArr[++i]);
+      } else if (newArr[i] === '--double-next' && i === (newArr.length -1)) {
+        newArr.splice(i, 1);
+      } else if (newArr[i] === '--double-prev' && i != 0) {
+        newArr.splice(i, 1, newArr[--i]);
+      } else if (newArr[i] === '--double-prev' && i === 0) {
+        newArr.splice(i, 1);
+      } 
+    } 
+    return newArr;
+  } else {
+    return Error("'arr' parameter must be an instance of the Array!");
+  }
 }
+
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]));
 
 module.exports = {
   transform
